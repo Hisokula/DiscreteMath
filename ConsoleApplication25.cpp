66553cedfb1e7aa4e8fbed6425516a52b0bbc33d
Hisokula <113204321+Hisokula@users.noreply.github.com>
2024-02-12 22:30:10 +0200

    Add files via upload

---
+++ ConsoleApplication25.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a = 1;
+    double b = 1;
+    for (int x = 2; x < 101; x++)
+    {
+        double next = 7 * -12 * a;
+        a = b;
+        b = next;
+    }
+    std::cout << std::fixed << b << std::endl;
+    std::cout << std::fixed << pow(-5, 100) / 7 + 6 * pow(2, 100) / 7 << std::endl;
+}
